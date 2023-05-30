dcc8c2eb04374c256fa7026b22caaa9daa9fae4b
Michael B. Espeña <michael.espena.public@gmail.com>
2023-05-30 23:35:08 +0800

    lesson 5

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,25 @@ function App() {
       <img class="rounded-md" src={banner} alt="site banner" />
 
       <div class="grid grid-cols-4 gap-10 my-4">
-        <Card />
-        <Card />
-        <Card />
+        <Card rounded={true} flat={false}>
+          <h2>Ninja Tee</h2>
+          <p>
+            Dolor ducimus porro sed sequi ipsum? Sapiente dolore et minima omnis
+            provident? Inventore ea ducimus.
+          </p>
+
+          <button class="btn">View</button>
+        </Card>
+
+        <Card rounded={false} flat={true}>
+          <h2>Ninja Hoodie</h2>
+          <p>
+            Dolor ducimus porro sed sequi ipsum? Sapiente dolore et minima omnis
+            provident? Inventore ea ducimus.
+          </p>
+
+          <button class="btn">View</button>
+        </Card>
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,10 @@
-export default function Card() {
+export default function Card(props) {
   return (
-    <div class="bg-white p-4 text-center rounded-md shadow-md">
-      <h2>Card Component</h2>
-      <p>
-        Adipisicing consequuntur rerum possimus doloremque cumque Impedit amet
-        tempora voluptates quos beatae sit modi. Ad?
-      </p>
-
-      <button class="btn">Click me!</button>
+    <div
+      class="bg-white p-4 text-center"
+      classList={{ "rounded-md": props.rounded, "shadow-md": !props.flat }}
+    >
+      {props.children}
     </div>
   );
 }
