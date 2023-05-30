516af6fb8e8a118c23ccd9f8c7d89b01c0a07417
Michael B. Espeña <michael.espena.public@gmail.com>
2023-05-30 23:17:18 +0800

    lesson 4

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
 export default function Card() {
   return (
-    <div>
+    <div class="bg-white p-4 text-center rounded-md shadow-md">
       <h2>Card Component</h2>
       <p>
         Adipisicing consequuntur rerum possimus doloremque cumque Impedit amet
         tempora voluptates quos beatae sit modi. Ad?
       </p>
+
+      <button class="btn">Click me!</button>
     </div>
   );
 }
